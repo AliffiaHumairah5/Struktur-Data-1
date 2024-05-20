44680ee86b54f6fa1d78e48936fca84f2a11378b
HP <HP@FTI-LAB03-PC12>
2024-05-20 09:17:27 +0700

    Pekan 4

--- PraktikumStrukturDataKelas1/src/Pekan2/StackPostfix.java
+++ PraktikumStrukturDataKelas1/src/Pekan2/StackPostfix.java
@@ -4,32 +4,33 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class StackPostfix {
-	public static int postfixEvaluate(String expression) {
+	
+	public static int postfixEvaluate (String expression) {
 		Stack<Integer> s = new Stack<Integer>();
-		try (Scanner input = new Scanner(expression)) {
-			while (input.hasNext()) {
-				if (input.hasNext()) {
-					s.push(input.nextInt());
-				}else {
-					String operator = input.next();
-					int operand2 = s.pop();
-					int operand1 = s.pop();
-					if (operator.equals("+")) {
-						s.push(operand1 + operand2);
-					}else if (operator.equals("-")) {
-						s.push(operand1 - operand2);
-					}else if (operator.equals("*")) {
-						s.push(operand1 * operand2);
-					}else {
-						s.push(operand1 / operand2);
-					}
-					}
-					}
+		Scanner input = new Scanner(expression);
+		while (input.hasNext()) {
+			if (input.hasNextInt()) {
+				s.push(input.nextInt());
+			} else { // an operator
+				String operator = input.next();
+				int operand2 = s.pop();
+				int operand1 = s.pop();
+				if (operator.equals("+")) {
+					s.push(operand1 + operand2);
+				} else if (operator.equals("-")) {
+					s.push(operand1 - operand2);
+				} else if (operator.equals("*")) {
+					s.push(operand1 * operand2);
+				} else {
+					s.push(operand1 / operand2);
+				}
+			}
 		}
 		return s.pop();
 	}
+
 	public static void main(String[] args) {
-	System.out.println("hasil postfix= "+postfixEvaluate("5 2 4 * + 7 -"));
+		System.out.println("hasil postfix = " + postfixEvaluate("5 2 4 * + 7 -"));
 
 	}
 
